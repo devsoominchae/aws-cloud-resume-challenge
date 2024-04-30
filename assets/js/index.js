85fd040a6de91c66cce0e837438dd1f5fd1b4a6c
103ho <smchae@globalbridge.co.kr>
2024-05-01 00:04:03 +0900

    Change API Gateway Endpoint

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 const counter = document.querySelector(".counter-number");
 async function updateCounter() {
-    let response = await fetch("https://6xy2i2ssxc.execute-api.ap-northeast-2.amazonaws.com/dev");
+    let response = await fetch("https://api.soominchae.net/views");
     let data = await response.json()
     console.log(data);
     counter.innerHTML = `Views : ${data}`
