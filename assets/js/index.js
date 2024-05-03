c234a2f254a976b80cb1c268119cc7c0dc59682c
Soo Min Chae <cothco0505@gmail.com>
2024-05-03 12:29:05 +0900

    Added today view count feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,21 @@
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch("https://api.soominchae.net/views");
+const total_counter = document.querySelector(".total-views");
+async function updateTotalViews() {
+    let response = await fetch("https://api.soominchae.net/getAndUpdateTotalViews");
+    console.log(response)
     let data = await response.json()
     console.log(data);
-    counter.innerHTML = `Views : ${data}`
+    total_counter.innerHTML = `Views : ${data}`
 }
 
-updateCounter()
+const today_counter = document.querySelector(".today-views");
+async function updateTodayViews() {
+    let response = await fetch("https://api.soominchae.net/getAndUpdateTodayCount");
+    console.log(response)
+    let data = await response.json()
+    console.log(data);
+    today_counter.innerHTML = `Today : ${data}`
+}
+
+updateTotalViews()
+
+updateTodayViews()
